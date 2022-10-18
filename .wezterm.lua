c1188fb5e1c3034a3337b128693d00c4469d12db
shouryaps <shourya4@gmail.com>
2022-10-19 05:02:43 +0530

    formatted file

--- .wezterm.lua
+++ .wezterm.lua
@@ -3,7 +3,7 @@ return {
   font = wezterm.font 'JetBrainsMono Nerd Font',
   font_size = 18.0,
   keys = {
-  	{key="LeftArrow", mods="OPT", action=wezterm.action{SendString="\x1bb"}},
-  	{key="RightArrow", mods="OPT", action=wezterm.action{SendString="\x1bf"}},
+    { key = "LeftArrow", mods = "OPT", action = wezterm.action { SendString = "\x1bb" } },
+    { key = "RightArrow", mods = "OPT", action = wezterm.action { SendString = "\x1bf" } },
   }
 }
