4540469127aba74df9468ba58ff45131cb1af822
shouryaps <shourya4@gmail.com>
2022-10-19 05:01:30 +0530

    added wezterm config

---
+++ .wezterm.lua
@@ -0,0 +1,9 @@
+local wezterm = require 'wezterm'
+return {
+  font = wezterm.font 'JetBrainsMono Nerd Font',
+  font_size = 18.0,
+  keys = {
+  	{key="LeftArrow", mods="OPT", action=wezterm.action{SendString="\x1bb"}},
+  	{key="RightArrow", mods="OPT", action=wezterm.action{SendString="\x1bf"}},
+  }
+}
